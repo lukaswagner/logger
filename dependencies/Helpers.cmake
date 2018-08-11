5301f3ae1ac77267f02bac4d0cfa655a2b46f7f6
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-08-12 00:18:23 +0200

    make msbuild shut up when building dependencies

--- dependencies/Helpers.cmake
+++ dependencies/Helpers.cmake
@@ -9,6 +9,17 @@ macro(clone dependency_name repo branch)
     execute_process(COMMAND git clone --depth 1 --branch ${branch} "${repo}" "${${dependency_name}_source_dir}")
 endmacro()
 
+macro(set_compiler_specific_flags)
+    # skip if already defined (if multiple dependencies are set up)
+    if(NOT compiler_specific_flags)
+        # check if VS is used - MSVC isn't defined since this project has language NONE
+        string(REGEX MATCH "^Visual Studio.*" using_vs ${CMAKE_GENERATOR})
+        if(using_vs)
+            set(compiler_specific_flags /m /verbosity:minimal)
+        endif()
+    endif()
+endmacro()
+
 # pass CMake options as additional args
 macro(build dependency_name)
     message(STATUS "Building ${dependency_name}")
@@ -20,12 +31,14 @@ macro(build dependency_name)
     # run CMake configuration - build type passed here is only used by single-conf generators
     execute_process(COMMAND ${CMAKE_COMMAND} ${ARGN} -DCMAKE_BUILD_TYPE=${SINGLE_CONFIGURATION_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${${dependency_name}_install_dir} -G "${CMAKE_GENERATOR}" ${${dependency_name}_source_dir} WORKING_DIRECTORY ${${dependency_name}_build_dir})
     # build and install - build type passed here is only used by multi-conf generators
+    # set some flags to optimize based on compiler
+    set_compiler_specific_flags()
     # this var is only defined when using multi-configuration  generators
     if(NOT CMAKE_CONFIGURATION_TYPES)
-        execute_process(COMMAND ${CMAKE_COMMAND} --build ${${dependency_name}_build_dir} --target install)
+        execute_process(COMMAND ${CMAKE_COMMAND} --build ${${dependency_name}_build_dir} --target install -- ${compiler_specific_flags})
     else()
         foreach(build_type IN ITEMS ${MULTI_CONFIGURATION_BUILD_TYPES})
-            execute_process(COMMAND ${CMAKE_COMMAND} --build ${${dependency_name}_build_dir} --target install --config "${build_type}")
+            execute_process(COMMAND ${CMAKE_COMMAND} --build ${${dependency_name}_build_dir} --target install --config "${build_type}" -- ${compiler_specific_flags})
         endforeach()
     endif()
 endmacro()

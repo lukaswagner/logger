bef7e98e93fd53b29d66b3a014c75d2636061f5a
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-09-08 16:33:25 +0200

    refs #12: allow ommiting the semicolon when using DEBUG_EXPR

--- include/macros.hpp
+++ include/macros.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#define DEBUG_EXPR(expression) logger::debug("DEBUG_EXPR") << #expression << " (" << __FILE__ << ":" << __LINE__ << ")"; expression
+#define DEBUG_EXPR(expression) logger::debug("DEBUG_EXPR") << #expression << " (" << __FILE__ << ":" << __LINE__ << ")";expression;
 
 #define DEBUG_VAL(value) logger::debug("DEBUG_VAL") << #value << " = " << (value);
